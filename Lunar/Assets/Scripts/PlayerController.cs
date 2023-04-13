4484cb3ff7325b9c3fdcb30a372f1a650ba38654
AlanCroda <78557195+AlanCroda@users.noreply.github.com>
2023-04-13 13:56:32 -0600

    Top down player movement

---
+++ Lunar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Vector2 movementInput;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        movementInput = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movementInput * speed;
+    }
+}
