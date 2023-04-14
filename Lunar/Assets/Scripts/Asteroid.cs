a7f9e9d0f84a77ba50cdb206562a1b6ce08f25e3
Imated <lineborder122@gmail.com>
2023-04-14 18:58:45 -0400

    Astroids + Bug Fixes

---
+++ Lunar/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace LunarJam
+{
+    public class Asteroid : Obstacle
+    {
+        [SerializeField] private float amplitude = 1f;
+        [SerializeField] private float frequency = 1f;
+        [SerializeField] private float sineSpeed = 25f;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            sineSpeed = Random.Range(sineSpeed - 2f, sineSpeed + 3f);
+        }
+
+        private void Update()
+        {
+            var newY = transform.position.y + amplitude * Mathf.Tan(Time.time * frequency);
+            var newVel = new Vector2(rb.velocity.x, (newY - transform.position.y) * sineSpeed);
+            rb.velocity = newVel;
+        }
+    }
+}
