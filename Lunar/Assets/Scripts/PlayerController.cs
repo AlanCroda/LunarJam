e058e09451d08d34bb098145c5f4d2af8ea3a56d
MYNAMEISRADU <38227348+MYNAMEISRADU@users.noreply.github.com>
2023-04-14 13:28:10 +0200

    changed movement

--- Lunar/Assets/Scripts/PlayerController.cs
+++ Lunar/Assets/Scripts/PlayerController.cs
@@ -2,10 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Windows;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
     private Vector2 movementInput;
 
     internal Rigidbody2D rb;
@@ -22,6 +25,14 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.velocity = movementInput * speed;
+        if (movementInput.magnitude > 0)
+        {
+            rb.velocity += movementInput * acceleration;
+            rb.velocity = new Vector2(Mathf.Clamp(rb.velocity.x, -speed, speed), Mathf.Clamp(rb.velocity.y, -speed, speed));
+        }
+        else
+        {
+            rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, Time.deltaTime * deceleration);
+        }
     }
 }
