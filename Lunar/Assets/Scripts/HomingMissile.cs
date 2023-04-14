e59effa17538ffa689f5e22fb7440859e30156fc
Imated <lineborder122@gmail.com>
2023-04-14 19:05:00 -0400

    Missles use Obstacle Base Class

--- Lunar/Assets/Scripts/HomingMissile.cs
+++ Lunar/Assets/Scripts/HomingMissile.cs
@@ -1,25 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace LunarJam
 {
-    public class HomingMissile : MonoBehaviour
+    public class HomingMissile : Obstacle
     {
-        private Rigidbody2D rb;
-        [SerializeField] private float speed;
         [SerializeField] private float homingStrength;
         private Transform target;
 
         private void Start()
         {
-            rb = GetComponent<Rigidbody2D>();
-            rb.velocity = -Vector3.right * speed;
-            target = GameObject.FindObjectOfType<PlayerController>().transform;
+            target = FindObjectOfType<PlayerController>().transform;
         }
-
-        // Update is called once per frame
-        void FixedUpdate()
+        
+        private void FixedUpdate()
         {
             Vector3 distance = target.position - transform.position;
             float forceMagnitude = homingStrength/(distance.magnitude * distance.magnitude);
