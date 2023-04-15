c2994f9ad3895c81a7a84698c2fdff18bcea1f7c
Imated <lineborder122@gmail.com>
2023-04-15 17:13:06 -0400

    Volume Slider

--- Lunar/Assets/Scripts/GameManager.cs
+++ Lunar/Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace LunarJam
 {
@@ -10,13 +12,21 @@ namespace LunarJam
     
     public class GameManager : MonoBehaviour
     {
+        [SerializeField] private Slider volumeSlider;
         public static GameManager instance;
         private GameState state;
+        private float volume;
 
         private void Awake()
         {
             instance = this;
             DontDestroyOnLoad(gameObject);
+            volumeSlider.onValueChanged.AddListener(OnVolumeSliderValueChanged);
+            SceneManager.sceneLoaded += (scene, mode) =>
+            {
+                AudioListener.volume = volume;
+            };
+            volume = 1;
         }
 
         public void SwitchGameStates(GameState newState)
@@ -27,6 +37,17 @@ namespace LunarJam
         public GameState GetState()
         {
             return state;
+        }
+
+        public float GetVolume()
+        {
+            return volume;
+        }
+
+        public void OnVolumeSliderValueChanged(float newValue)
+        {
+            volume = newValue;
+            AudioListener.volume = volume;
         }
     }
 }
