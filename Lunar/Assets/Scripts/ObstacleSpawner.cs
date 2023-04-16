af65354b418396408afcf024a3263fbaf7dfc9b6
Imated <lineborder122@gmail.com>
2023-04-16 12:59:58 -0400

    Weighted lists + More Presets

--- Lunar/Assets/Scripts/ObstacleSpawner.cs
+++ Lunar/Assets/Scripts/ObstacleSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     public static ObstacleSpawner instance;
-    [SerializeField] private List<GameObject> levelPresets;
+    [SerializeField] private WeightedGameObjectList levelPresets;
     [SerializeField] private int maxAreas = 5;
 
     private List<GameObject> spawnedAreas = new List<GameObject>();
@@ -28,8 +28,7 @@ public class ObstacleSpawner : MonoBehaviour
         var pos = new Vector3(25f, 0f, 0);
         if (spawnedAreas.Count > 0)
             pos = new Vector3(spawnedAreas[spawnedAreas.Count - 1].transform.position.x + 15f, 0f, 0f);
-        var rand = Random.Range(0, levelPresets.Count);
-        var obj = Instantiate(levelPresets[rand], pos, Quaternion.identity);
+        var obj = Instantiate(levelPresets.GetRandomObject(), pos, Quaternion.identity);
         spawnedAreas.Add(obj);
     }
 }
