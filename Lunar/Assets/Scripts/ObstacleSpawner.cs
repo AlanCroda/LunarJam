12674f15fe78929ac36bf27ba04cad0e6fe4ccc3
Imated <lineborder122@gmail.com>
2023-04-16 12:13:56 -0400

    Fixed Arcade Generation

--- Lunar/Assets/Scripts/ObstacleSpawner.cs
+++ Lunar/Assets/Scripts/ObstacleSpawner.cs
@@ -27,14 +27,9 @@ public class ObstacleSpawner : MonoBehaviour
     {
         var pos = new Vector3(25f, 0f, 0);
         if (spawnedAreas.Count > 0)
-            pos = new Vector3(spawnedAreas[spawnedAreas.Count - 1].transform.position.x + 10f, 0f, 0f);
+            pos = new Vector3(spawnedAreas[spawnedAreas.Count - 1].transform.position.x + 15f, 0f, 0f);
         var rand = Random.Range(0, levelPresets.Count);
         var obj = Instantiate(levelPresets[rand], pos, Quaternion.identity);
         spawnedAreas.Add(obj);
-        if (spawnedAreas.Count > maxAreas)
-        {
-            Destroy(spawnedAreas[spawnedAreas.Count - 1]);
-            spawnedAreas.RemoveAt(spawnedAreas.Count - 1);
-        }
     }
 }
